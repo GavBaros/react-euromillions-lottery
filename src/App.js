f226e3b971186b1b848e47ef2a8a4b08d96f5a63
Gav Barosee <barbarosee@gmail.com>
2019-04-07 12:50:03 +0100

    Add PlayLine

--- src/App.js
+++ src/App.js
@@ -1,80 +1,17 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import PlayLine from "./components/PlayLine";
 import "./App.css";
-
-const initialState = {
-  input1: "",
-  input2: "",
-  input3: "",
-  input4: "",
-  input5: ""
-};
 
 class App extends Component {
   state = {};
 
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value }, () =>
-      console.log(this.state)
-    );
-  };
-
-  clearAllInputs = () => {
-    this.setState(initialState);
-  };
-
-  populateInputsWithRandomNumbers = () => {
-    const changedInitialState = this.changeInitialStateValues();
-    this.setState(changedInitialState);
-  };
-
-  changeInitialStateValues = () => {
-    let initialStateCopy = { ...initialState };
-
-    Object.keys(initialStateCopy).forEach(
-      key => (initialStateCopy[key] = Math.floor(Math.random() * 50 + 1))
-    );
-
-    return initialStateCopy;
-  };
-
   render() {
     return (
       <div className="App">
-        <button onClick={this.populateInputsWithRandomNumbers}>
-          Lucky Dip
-        </button>
-        <input
-          type="text"
-          name="input1"
-          value={this.state.input1}
-          onChange={this.handleChange}
-        />
-        <input
-          type="text"
-          name="input2"
-          value={this.state.input2}
-          onChange={this.handleChange}
-        />
-        <input
-          type="text"
-          name="input3"
-          value={this.state.input3}
-          onChange={this.handleChange}
-        />
-        <input
-          type="text"
-          name="input4"
-          value={this.state.input4}
-          onChange={this.handleChange}
-        />
-        <input
-          type="text"
-          name="input5"
-          value={this.state.input5}
-          onChange={this.handleChange}
-        />
-        <span onClick={this.clearAllInputs}>&times;</span>
+        <PlayLine />
+        <PlayLine />
+        <PlayLine />
+        <PlayLine />
       </div>
     );
   }
---
+++ src/components/PlayLine.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+
+class PlayLine extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+
+    this.clearedState = {
+      input_1: "",
+      input_2: "",
+      input_3: "",
+      input_4: "",
+      input_5: "",
+      input_6: "",
+      input_7: ""
+    };
+
+    this.inputs = [1, 2, 3, 4, 5, 6, 7];
+  }
+
+  populateInputsWithRandomNumbers = () => {
+    const changedClearedState = this.changeClearedStateValues();
+    this.setState(changedClearedState);
+  };
+
+  changeClearedStateValues = () => {
+    let clearedStateCopy = { ...this.clearedState };
+
+    Object.keys(clearedStateCopy).forEach(
+      key => (clearedStateCopy[key] = Math.floor(Math.random() * 50 + 1))
+    );
+
+    return clearedStateCopy;
+  };
+
+  handleLuckyDipClick = () => {
+    this.setState({ luckyDipClicked: true });
+
+    if (this.state.luckyDipClicked) {
+      return this.clearAllInputs();
+    }
+    return this.populateInputsWithRandomNumbers();
+  };
+
+  clearAllInputs = () =>
+    this.setState({ ...this.clearedState, luckyDipClicked: false });
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  renderInputs = () => {
+    return this.inputs.map(num => (
+      <input
+        key={num}
+        type="text"
+        name={`input_${num}`}
+        value={this.state[`input_${num}`]}
+        onChange={this.handleChange}
+      />
+    ));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.handleLuckyDipClick}>Lucky Dip</button>
+        {this.renderInputs()}
+        <span onClick={this.clearAllInputs}>&times;</span>
+      </div>
+    );
+  }
+}
+
+export default PlayLine;
